1a29bbc6ce94efb8907eff5325f7b649fd666a8d
Inti Brito <intbrd@yahoo.com>
2025-04-23 12:35:59 -0500

    Update eslint.config.js to use languageOptions.parser

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,19 @@
-module.exports = {
+import globals from 'globals';
+import security from 'eslint-plugin-security';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+
+export default [{
   languageOptions: {
-    globals: {
-      node: true
-    }
+    globals: globals.node,
+    parser: parser
   },
-  parser: '@typescript-eslint/parser',
+  plugins: {
+    security: security,
+    '@typescript-eslint': typescriptEslint
+  },
   extends: [
     'plugin:security/recommended',
     'plugin:@typescript-eslint/recommended'
-  ],
-  plugins: [
-    'security',
-    '@typescript-eslint'
   ]
-};
+}];
